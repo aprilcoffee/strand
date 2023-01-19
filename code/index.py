a1af93bdb5ef52623dd2cdcb7f5edd4be9aee1d3
Ting Chun Liu <8765498@gmail.com>
2023-01-19 10:52:38 +0100

    add new

--- code/index.py
+++ code/index.py
@@ -15,22 +15,24 @@ os.environ['STABILITY_KEY'] = config.stabilityAPI
 stability_api = client.StabilityInference(
     key=os.environ['STABILITY_KEY'], # API Key reference.
     verbose=True, # Print debug messages.
-    engine="stable-diffusion-v1-5", # Set the engine to use for generation. For SD 2.0 use "stable-diffusion-v2-0".
-)
+    # Set the engine to use for generation. For SD 2.0 use "stable-diffusion-v2-0".
+    engine="stable-diffusion-768-v2-1"
+    #engine="stable-diffusion-v1-5", 
+    )
 
 def generate(_prompt, _img):
     answers = stability_api.generate(
         prompt=_prompt,
         init_image=_img, # Assign our previously generated img as our Initial Image for transformation.
         start_schedule=0.6, # Set the strength of our prompt in relation to our initial image.
-        seed=123467458, # If attempting to transform an image that was previously generated with our API,
+        #seed=123467458, # If attempting to transform an image that was previously generated with our API,
                         # initial images benefit from having their own distinct seed rather than using the seed of the original image generation.
         steps=30, # Amount of inference steps performed on image generation. Defaults to 30. 
         cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
                     # Setting this value higher increases the strength in which it tries to match your prompt.
                     # Defaults to 7.0 if not specified.
-        width=512, # Generation width, defaults to 512 if not included.
-        height=512, # Generation height, defaults to 512 if not included.
+        width=1024, # Generation width, defaults to 512 if not included.
+        height=1024, # Generation height, defaults to 512 if not included.
         sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
     )
     for resp in answers:
@@ -51,4 +53,5 @@ imageDir.mkdir(parents=True, exist_ok=True)
 
 
 img = Image.open("code/images/kinmen.png")
-generate("island",img).save(imageDir / "output.png") 
+for i in range(5):
+    generate("",img).save(imageDir / ("output_new_"+str(i)+".png"))
